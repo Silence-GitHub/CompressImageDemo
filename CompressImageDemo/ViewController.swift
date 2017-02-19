9533cd1995344338f80cda588f0b6422a69a856d
Kaibo Lu <lukaibolkb@gmail.com>
2017-02-19 19:10:56 +0800

    Initial Commit

---
+++ CompressImageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompressImageDemo
+//
+//  Created by Kaibo Lu on 2017/2/19.
+//  Copyright © 2017年 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
